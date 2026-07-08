c50e9965d3a6b691af1e881e9940f4245c0ddc67
Perkusrak797 <gerikolesnikov8@gmail.com>
2026-07-08 16:48:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: "/3_months_together/",
+      base: "/3_moths_together/",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
